f5e1b965a82f483b0de4721b6ccc8a585b270ee1
Scott Pantall <aspantall@gmail.com>
2017-05-23 18:18:57 -0600

    Works on Code Eval

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
  * @author Scott Pantall
  */
 import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.file.*;
 
 public class Main {
@@ -19,5 +20,35 @@ public class Main {
     }
     
     public static void selfDescribe(String line) {
-        System.out.println(line);
+        char[] numbers = line.toCharArray();
+        int[] digits = new int[numbers.length];
+        
+        for(int i = 0; i < digits.length; i++) {
+            digits[i] = Character.getNumericValue(numbers[i]);
+        }
+        
+        // Read the number in the position
+        // Check the full array to see how many times the number for that position occurs
+        // If the number in the position equals the number of times, then true and check next position
+        // If not, then false and stop
+        
+        for(int i = 0; i < digits.length; i++) {
+            int positionValue = digits[i];
+            int count = 0;
+            
+            for(int j = 0; j < digits.length; j++) {
+                if(digits[j] == i) {
+                    count++;
+                }
+            }
+            
+            if(positionValue != count) {
+                System.out.println("0");
+                return;
+            }
+        }
+        
+        System.out.println("1");
+        return;
+    }
 }
